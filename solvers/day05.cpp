799e572fb2a36ade0dd21462722186a1d9c90836
Adrien Allard <adrien.allard.pro@gmail.com>
2022-12-24 00:17:05 +0100

    Fixed: Compilation warnings

--- solvers/day05.cpp
+++ solvers/day05.cpp
@@ -68,7 +68,7 @@ std::string day05_part1(std::ifstream& input)
             int from = stoi(matches[2]);
             int to = stoi(matches[3]);
 
-            for (uint32_t c = 0; c < count; ++c)
+            for (int c = 0; c < count; ++c)
             {
                 stacks[to].push(stacks[from].top());
                 stacks[from].pop();
@@ -155,7 +155,7 @@ std::string day05_part2(std::ifstream& input)
             int to = stoi(matches[3]);
 
             std::stack<char> buf;
-            for (uint32_t c = 0; c < count; ++c)
+            for (int c = 0; c < count; ++c)
             {
                 buf.push(stacks[from].top());
                 stacks[from].pop();
